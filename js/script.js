1aa059a26bcef9c9d30d0734b634e21fc83e7f48
David <Davidgarciama@outlook.com>
2022-03-24 12:05:36 -0600

    first cube bar animation

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// hamburger button
+
 const HAMBURGER_BUTTON = document.getElementById("hamburger-button")
 const MENU_OPENED = document.getElementById("menu")
 const showMenu = () => {
@@ -5,3 +7,15 @@ const showMenu = () => {
     MENU_OPENED.classList.toggle("menu-opened")
 }
 HAMBURGER_BUTTON.addEventListener("click", showMenu)
+
+
+// 3d cube
+
+const THREED_CUBE = document.getElementById("threed-cube__cube")
+let cssBar = document.getElementById("css__text")
+let barCss = document.getElementById("bar__css")
+const flexbox = () => {
+    cssBar.innerHTML="<span class=\"title\">FLEXBOX</span> <span>80%</span>"
+    barCss.classList.add("bar__flexbox")
+}
+THREED_CUBE.addEventListener("animationend", flexbox)
