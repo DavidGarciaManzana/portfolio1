100864df9188ff4b493c36643eb2b1109416d8dd
David <Davidgarciama@outlook.com>
2022-04-22 17:29:47 -0500

    Portfolio project finished

--- js/script.js
+++ js/script.js
@@ -155,7 +155,7 @@ THREED_CUBE.addEventListener("animationend", jsBar)
 const responsiveGallery = document.getElementById("responsive-gallery")
 
 let projects = [
-    ["../media/images/portfolio-web.webp", "../media/images/cat.webp", "../media/images/portfolio-light.webp", "Web", "Portfolio", "01/04/2022", "https://davidgarxa.com", "HTML, CSS(SASS), Flexbox, Grid, Animations, RWD & JS", "My personal portfolio where I show all my projects and what technologies I like to implement to them."],
+    ["../media/images/portfolio-web.webp", "../media/images/portfolio-mobile.webp", "../media/images/portfolio-light.webp", "Web", "Portfolio", "01/04/2022", "https://davidgarxa.com", "HTML, CSS(SASS), Flexbox, Grid, Animations, RWD & JS", "My personal portfolio where I show all my projects and what technologies I like to implement to them."],
     ["../media/images/homer.webp", "../media/images/homer-mobile.webp", "../media/images/homer-light.webp", "Exercise", "Homer", "17/11/2021", "https://davidgarxa.com/homer", "HTML & CSS", "Draw of homer using HTML & CSS"],
     ["../media/images/dmm-web.webp", "../media/images/dmm-mobile.webp", "../media/images/dmm-light.webp", "Web", "Drilling Mud Mex", "17/02/2022", "https://davidgarxa.com/drilling-mud-mex/", "HTML, CSS, Flexbox, Animations & JS", "Petroleum Engineering web app that accurately describe the addition of weight material to clay/water muds."],
     ["../media/images/profile-web.webp", "../media/images/profile-mobile.webp", "../media/images/profile-light.webp", "Exercise", "Profile Card", "02/09/2021", "https://davidgarxa.com/profile-card", "HTML & CSS", "A profile card made with HTML & CSS"],
