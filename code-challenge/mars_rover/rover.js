47093c56e4e4bd01039f2c7a2ea10533023d7dd4
David <Davidgarciama@outlook.com>
2022-06-03 11:24:14 -0500

    Fixed

--- code-challenge/mars_rover/rover.js
+++ code-challenge/mars_rover/rover.js
@@ -7,15 +7,32 @@ let B = 1
 let rover = document.getElementById("rover")
 
 
-let play = () => {
+let moveSound = () => {
     let audio = new Audio("media/sounds/sound.wav")
     audio.play()
 }
 
-let moveSound =()=>{
+let rotateSound =()=>{
     let audio = new Audio("media/sounds/rotate.wav")
     audio.play()
 }
+
+let marsIsASphere = (position) => {
+    if (position[0] === -1) {
+        roverPosition[0] = 10
+    }
+    if (position[1] === -1) {
+        roverPosition[1] = 10
+    }
+    if (position[0] === 11) {
+        roverPosition[0] = 0
+    }
+    if (position[1] === 11) {
+        roverPosition[1] = 0
+    }
+}
+
+
 //---------------------------------------MOVE THE ROVER
 let up = document.getElementById("up")
 let down = document.getElementById("down")
@@ -28,18 +45,20 @@ let move = (direction, move) => {
     let roverPositionMoveW = roverPositionWidth.slice(0, -3)
     if (direction === "N") {
         if (move === "F") {
-            play()
+            moveSound()
             roverPosition[1] += F
             rover.style.bottom = Number(roverPositionMoveH) + 1.5 + "rem";
+            marsIsASphere(roverPosition)
             if (roverPositionMoveH > 14) {
                 rover.style.bottom = 0.5 + "rem"
             }
             return roverPosition
         }
         if (move === "B") {
-            play()
+            moveSound()
             roverPosition[1] -= B
             rover.style.bottom = Number(roverPositionMoveH) - 1.5 + "rem";
+            marsIsASphere(roverPosition)
             if (roverPositionMoveH < 1) {
                 rover.style.bottom = 15.5 + "rem"
             }
@@ -48,18 +67,20 @@ let move = (direction, move) => {
     }
     if (direction === "S") {
         if (move === "F") {
-            play()
+            moveSound()
             roverPosition[1] -= F
             rover.style.bottom = Number(roverPositionMoveH) - 1.5 + "rem";
+            marsIsASphere(roverPosition)
             if (roverPositionMoveH < 1) {
                 rover.style.bottom = 15.5 + "rem"
             }
             return roverPosition
         }
         if (move === "B") {
-            play()
+            moveSound()
             roverPosition[1] += B
             rover.style.bottom = Number(roverPositionMoveH) + 1.5 + "rem";
+            marsIsASphere(roverPosition)
             if (roverPositionMoveH > 14) {
                 rover.style.bottom = 0.5 + "rem"
             }
@@ -68,18 +89,20 @@ let move = (direction, move) => {
     }
     if (direction === "E") {
         if (move === "F") {
-            play()
+            moveSound()
             roverPosition[0] += F
             rover.style.left = Number(roverPositionMoveW) + 1.5 + "rem";
+            marsIsASphere(roverPosition)
             if (roverPositionMoveW > 13) {
                 rover.style.left = -0.7 + "rem"
             }
             return roverPosition
         }
         if (move === "B") {
-            play()
+            moveSound()
             roverPosition[0] -= B
             rover.style.left = Number(roverPositionMoveW) - 1.5 + "rem";
+            marsIsASphere(roverPosition)
             if (roverPositionMoveW < 0) {
                 rover.style.left = 14.3 + "rem"
             }
@@ -88,18 +111,20 @@ let move = (direction, move) => {
     }
     if (direction === "W") {
         if (move === "F") {
-            play()
+            moveSound()
             roverPosition[0] -= F
             rover.style.left = Number(roverPositionMoveW) - 1.5 + "rem";
+            marsIsASphere(roverPosition)
             if (roverPositionMoveW < 0) {
                 rover.style.left = 14.3 + "rem"
             }
             return roverPosition
         }
         if (move === "B") {
-            play()
+            moveSound()
             roverPosition[0] += B
             rover.style.left = Number(roverPositionMoveW) + 1.5 + "rem";
+            marsIsASphere(roverPosition)
             if (roverPositionMoveW > 13) {
                 rover.style.left = -0.7 + "rem"
             }
@@ -122,48 +147,48 @@ let right = document.getElementById("right");
 let rotate = (actualDirection = "N", rotateDirection = R) => {
     if (actualDirection === "N") {
         if (rotateDirection === "R") {
-            moveSound()
+            rotateSound()
             rover.style.transform = "rotate(90deg)";
             return roverDirection = "E"
         }
         if (rotateDirection === "L") {
-            moveSound()
+            rotateSound()
             rover.style.transform = "rotate(-90deg)";
             return roverDirection = "W"
         }
     }
     if (actualDirection === "E") {
         if (rotateDirection === "R") {
-            moveSound()
+            rotateSound()
             rover.style.transform = "rotate(180deg)";
             return roverDirection = "S"
         }
         if (rotateDirection === "L") {
-            moveSound()
+            rotateSound()
             rover.style.transform = "rotate(0deg)";
             return roverDirection = "N"
         }
     }
     if (actualDirection === "S") {
         if (rotateDirection === "R") {
-            moveSound()
+            rotateSound()
             rover.style.transform = "rotate(-90deg)";
             return roverDirection = "W"
         }
         if (rotateDirection === "L") {
-            moveSound()
+            rotateSound()
             rover.style.transform = "rotate(90deg)";
             return roverDirection = "E"
         }
     }
     if (actualDirection === "W") {
         if (rotateDirection === "R") {
-            moveSound()
+            rotateSound()
             rover.style.transform = "rotate(0deg)";
             return roverDirection = "N"
         }
         if (rotateDirection === "L") {
-            moveSound()
+            rotateSound()
             rover.style.transform = "rotate(180deg)";
             return roverDirection = "S"
         }
@@ -178,8 +203,8 @@ left.addEventListener("click", () => {
 //---------------------------WHERES THE ROVER
 
 let mayday = document.getElementById("show-data")
-let showData = () => {
+let showData =()=> {
     alert(`Rover position: ${roverPosition}`)
     alert(`Rover direction: ${roverDirection}`)
 }
-mayday.addEventListener("click", showData)
+mayday.addEventListener("click",showData)
